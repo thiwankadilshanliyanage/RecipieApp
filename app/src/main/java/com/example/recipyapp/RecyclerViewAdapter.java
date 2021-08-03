68e5bea7437285ca6da2f4fcbb07129169a2195f
thiwankadilshanliyanage <thiwankadilshanhub98@gmail.com>
2021-08-03 11:31:04 +0530

    Successfully commit

---
+++ app/src/main/java/com/example/recipyapp/RecyclerViewAdapter.java
@@ -0,0 +1,44 @@
+package com.example.recipyapp;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyHolder> {
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MyHolder extends RecyclerView.ViewHolder {
+
+        TextView recipeTitle;
+        CardView cardView;
+        ImageView img_recipe_thumbnail;
+
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+
+            recipeTitle =(TextView)itemView.findViewById(R.id.text_recipe);
+            img_recipe_thumbnail = (ImageView)itemView.findViewById(R.id.recipe_img_id);
+            cardView = (CardView)itemView.findViewById(R.id.cardView);
+
+        }
+    }
+}
